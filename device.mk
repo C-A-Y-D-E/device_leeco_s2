d0b80d5f43baeb6de4b69179805779039b669fad
Proprk <30632833+Proprk@users.noreply.github.com>
2018-01-21 17:26:33 +0530

    Added wcnss_service
    Chutiyapa hora mkc

--- device.mk
+++ device.mk
@@ -315,6 +315,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/wifi/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+
+PRODUCT_PACKAGES += \
+    wcnss_service
+    
 # Misc
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
